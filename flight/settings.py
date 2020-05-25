118986ea205b3985a8759ab2a839ae2371c0dabc
MIRACLE <nnabukomiracle@gmail.com>
2020-05-25 14:36:06 +0100

    updated requirements.txt

--- flight/settings.py
+++ flight/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mq_ax_+sau*o(rw^6(7++13jnd%m#im!5ytk2z&$_a+(v5-b-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','192.168.113.12']
 
